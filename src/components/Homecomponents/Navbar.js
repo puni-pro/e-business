3f5c23fc1de8fb313981808dbeb825dfb68d4935
pocky <pocky1111gm@gmail.com>
2023-12-03 15:01:20 +0900

    add popup

--- src/components/Homecomponents/Navbar.js
+++ src/components/Homecomponents/Navbar.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import PopUp from './Navbarcomponents/PopUp';
 
 const Navbar = () => {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    };
+
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <NavLink to="/" >
                     <p className="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white">Autorutuku</p>
                 </NavLink>
-                <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add data</button>
-
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button onClick={togglePopup} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add data</button>
+                    {showPopup && (
+                        <PopUp togglePopup = {togglePopup} />
+                    )}
                 </div>
-
 
                 <div className="flex md:order-2">
                     <div className="relative hidden md:block">
